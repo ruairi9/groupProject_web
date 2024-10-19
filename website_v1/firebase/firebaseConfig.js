d4cdcbe9ea66d3d6b2cc8778f89aacc40d7c5615
ruairi9 <ruairi.walsh233@gmail.com>
2024-10-19 17:30:36 +0100

    Update firebaseConfig.js

--- website_v1/firebase/firebaseConfig.js
+++ website_v1/firebase/firebaseConfig.js
@@ -6,13 +6,4 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBL_2nfEkNycBr_VlF3K2sqLxUkk6bKpzc",
-  authDomain: "group-project-af653.firebaseapp.com",
-  databaseURL: "https://group-project-af653-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "group-project-af653",
-  storageBucket: "group-project-af653.appspot.com",
-  messagingSenderId: "217879299324",
-  appId: "1:217879299324:web:5263f3dff4358bd0650379",
-  measurementId: "G-SL1TS9GSCB"
-};
+
